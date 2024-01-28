e0ee2b67d29b2a59dc92ba643bab949ddc557891
wally1997 <walterbolivar97@gmail.com>
2024-01-28 12:38:34 -0500

    mmm

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,5 @@
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { tslib }  from 'tslib';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
